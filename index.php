0d69e118a2626d85c31e1a430cbd8b0b5aa6fef1
viorel4321 <brutasviorel@gmail.com>
2022-12-31 17:57:05 +0200

    comentariu

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+salutare  
+   <?php
+   echo ("salut Leroiv");
+   echo "inca un comment";
+   echo "comnts";
+   
+   
+   ?>
+   
+
+
+  </body>
+</html>
