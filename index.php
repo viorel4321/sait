7ae8aac088e3285f4f6b0c422d0d0e9f95c8d70f
viorel4321 <brutasviorel@gmail.com>
2023-01-01 10:57:09 +0200

    baza

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-salutare  
+  
    <?php
-   echo ("salut Leroiv");
-   echo "inca un comment";
-   echo "comnts";
-   
-   echo "asdsadadadd";
-   echo "asdsadadsdfsfsdfsadd";
-echo 4564;
+ 
+ $connect = mysqli_connect("localhost", "admin", "leroiv461", "essence3");
 
+ $query = "select * from tabel";
+ 
+ $result = mysqli_query($connect, $query);
+ 
+ echo mysqli_num_rows($result);
+
+ $row = mysqli_fetch_assoc($result);
+echo $row['descriere'];
+
+printf ("%s (%s) %s \n", $row["id"], $row["descriere"], $row["id"]);
    ?>
    
 
